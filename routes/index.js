c29866fc65d7dc7c01220bc3d1b467b6b0217828
Daniel Sorto <danielesorto@gmail.com>
2015-06-18 13:46:19 -0600

    Adicion de vista author

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 //Se importa el controlador al router index
 var quizController=require('../controller/quiz_controller');
+var authorController=require('../controller/author_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
@@ -9,4 +10,5 @@ router.get('/', function(req, res, next) {
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+router.get('/author',authorController.author);
 module.exports = router;

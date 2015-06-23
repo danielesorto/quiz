628d3807264f0c9ae0187821fab52a8e182630ef
Daniel Sorto <danielesorto@gmail.com>
2015-06-23 11:55:32 -0600

    Lista de preguntas y autoload

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,12 @@ var authorController=require('../controller/author_controller');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+//AutoLoad de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+//Definicion de rutas de /quizes
+router.get('/quizes', quizController.index );
+router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/author',authorController.author);
 module.exports = router;

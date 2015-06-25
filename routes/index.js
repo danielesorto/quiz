efebfb5f86823acd6638d77b6bf0bfc34949cde6
Daniel Sorto <danielesorto@gmail.com>
2015-06-25 11:55:44 -0600

    Actualizacion de Orden de Rutas

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,11 @@ router.get('/', function(req, res, next) {
 router.param('quizId', quizController.load); //autoload :quizId
 
 //Definicion de rutas de /quizes
-router.get('/quizes/:search?', quizController.index );
-//router.get('/quizes/:search?', quizController.index );
+
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
+router.get('/quizes/new',quizController.new);
+router.post('/quizes/create',quizController.create);
+router.get('/quizes/:search?', quizController.index );
 router.get('/author',authorController.author);
 module.exports = router;
